8f8b7abe2bad8fe1c099e4a6cd79f4f853610dbc
abishekanthony07 <abishekanthony07@gmail.com>
2019-09-28 21:17:15 +0200

    HEADER-html-TAG
    Eigenen h1 erstellt, weil h1 von html Probleme gemacht hat.
    Weiterhin wurde noch bei einem onclick auf das Bild die entsprechende Methode azsgeführt.
    index.js beinhaltet das ein und ausblenden der Buttons sowie auch das rotieren des Pfeils
    Zudem wurde noch das stylesheet etwas angepasst und strukturiert.

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+let buttonsSindZusehen = false;
+
+function animateArrow() {
+    let arrowDown = document.getElementById('arrowDown');
+    let auswahlMenue = document.getElementById('auswahlMenue');
+
+    if (!buttonsSindZusehen) {
+        rotateImage(arrowDown, 'rotate(-180deg)');
+        buttonsSindZusehen = true;
+        auswahlMenue.style.display = 'inline-block';
+    } else {
+        rotateImage(arrowDown, 'rotate(0deg)');
+        buttonsSindZusehen = false;
+        auswahlMenue.style.display = 'none';
+    }
+}
+
+function rotateImage(img, degree) {
+    img.style.transform = degree;
+    img.style.WebkitTransitionDuration = '0.5s';
+}
