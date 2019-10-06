c9e933f4e1561e5cfa2453af55ff2915b19639c1
Sascha Klevenhaus <saschaklevenhaus@gmail.com>
2019-10-06 14:39:21 +0200

    Kleinere Veränderungen an .js

--- javascript/bmiRechner.js
+++ javascript/bmiRechner.js
@@ -1,24 +1,21 @@
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
     bmiBerechnen();
 });
 
 function bmiBerechnen() {
     let buttonBerechen = document.getElementById('berechnenButton');
-    let eingabeGroesse = document.getElementById("groesse").value;
-    let eingabeGewicht = document.getElementById("masse").value;
     buttonBerechen.addEventListener("click", () => {
-            let ergebnis = eingabeGewicht / Math.pow(eingabeGroesse/100, 2);
+            let eingabeGroesse = document.getElementById("groesse").value;
+            let eingabeGewicht = document.getElementById("masse").value;
+            let ergebnis = eingabeGewicht / Math.pow(eingabeGroesse / 100, 2);
+            let anzeige = document.getElementById("ausgabe").innerText = "" + ergebnis;
 
         }
     )
 };
 
+function gleichsetzen() {
 
-// let berechnenButton;
-//
-// berechnenButton.addEventListener(bmiBerechnen(eingabeGewicht, eingabeGroesse));
-//
-//
-// console.log(eingabeGewicht);
-// console.log(eingabeGroesse);
+}
+
 
