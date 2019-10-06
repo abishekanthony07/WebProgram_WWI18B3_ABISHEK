5b5f77e54ab979eebd63ac7a9b1425c84ef45387
Sascha Klevenhaus <saschaklevenhaus@gmail.com>
2019-10-06 17:55:10 +0200

    Tabelle hinzugefügt, Button "BMI berechnen" hinzugefügt und  bearbeitet, Berechnung bearbeitet, CSS angepasst

--- javascript/bmiRechner.js
+++ javascript/bmiRechner.js
@@ -1,21 +1,22 @@
 window.addEventListener("load", () => {
-    bmiBerechnen();
+    let buttonBerechen = document.getElementById('berechnenButton');
+    buttonBerechen.addEventListener("click", () => {
+        bmiBerechnen();
+    });
+    window.addEventListener("keypress", (p) => {
+        if (p.key == "Enter") {
+            bmiBerechnen();
+        }
+    });
 });
 
 function bmiBerechnen() {
-    let buttonBerechen = document.getElementById('berechnenButton');
-    buttonBerechen.addEventListener("click", () => {
-            let eingabeGroesse = document.getElementById("groesse").value;
-            let eingabeGewicht = document.getElementById("masse").value;
-            let ergebnis = eingabeGewicht / Math.pow(eingabeGroesse / 100, 2);
-            let anzeige = document.getElementById("ausgabe").innerText = "" + ergebnis;
-
-        }
-    )
+    let eingabeGroesse = document.getElementById("groesse").value;
+    let eingabeGewicht = document.getElementById("masse").value;
+    let ergebnis = eingabeGewicht / Math.pow(eingabeGroesse / 100, 2);
+    let anzeige = document.getElementById("ausgabe");
+    ergebnis=ergebnis.toFixed(2);
+    anzeige.innerHTML = "Dein BMI ist: " + ergebnis + ".";
+    anzeige.style.display = 'flex';
 };
-
-function gleichsetzen() {
-
-}
-
 
