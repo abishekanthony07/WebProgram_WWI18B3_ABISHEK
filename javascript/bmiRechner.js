882ea4382994d7c3290e51e454fe83539135beae
Sascha Klevenhaus <saschaklevenhaus@gmail.com>
2019-10-07 19:55:49 +0200

    Tabelle angepasst, if-Anweisung für ergebnis Abfrage erstellt, Verlinkung zu den anderen Seiten sichergestellt, Ausgabe vom ergebnis überarbeitet

--- javascript/bmiRechner.js
+++ javascript/bmiRechner.js
@@ -16,7 +16,31 @@ function bmiBerechnen() {
     let ergebnis = eingabeGewicht / Math.pow(eingabeGroesse / 100, 2);
     let anzeige = document.getElementById("ausgabe");
     ergebnis=ergebnis.toFixed(2);
-    anzeige.innerHTML = "Dein BMI ist: " + ergebnis + ".";
+    hintergrundAngleichen(ergebnis, anzeige);
+    ergebnis = " " + ergebnis.bold();
+    anzeige.innerHTML =" <b>Dein BMI ist: </b> &nbsp;"+ ergebnis + "<b>.</b>";
     anzeige.style.display = 'flex';
+
 };
+
+function hintergrundAngleichen(ergebnis, anzeige){
+    if (ergebnis < 16){
+        anzeige.style.backgroundColor = "#7c7cbc";
+    }else if(ergebnis > 16 && ergebnis < 17){
+        anzeige.style.backgroundColor = "#7c7cfc";
+    }else if(ergebnis > 17 && ergebnis < 18.5){
+        anzeige.style.backgroundColor = "#7cfcfc";
+    }else if(ergebnis > 18.5 && ergebnis < 25){
+        anzeige.style.backgroundColor = "#7cfc7c";
+    }else if(ergebnis > 25 && ergebnis < 30){
+        anzeige.style.backgroundColor = "#fcfc7c";
+    }else if(ergebnis > 30 && ergebnis < 35){
+        anzeige.style.backgroundColor = "#fcbb91";
+    }else if(ergebnis > 35 && ergebnis < 40){
+        anzeige.style.backgroundColor = "#fc9191";
+    }else if(ergebnis >= 40){
+        anzeige.style.backgroundColor = "#c08080";
+    }
+
+}
 
