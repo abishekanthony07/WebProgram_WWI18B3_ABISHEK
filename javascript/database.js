2cc0935590f354431848f099c5faf80c3ddd58e0
abishekanthony07 <abishekanthony07@gmail.com>
2019-10-07 17:48:38 +0200

    Datenbankverknüpfung zu Firebase erfolgreich erstellt.
    //Todo
    Login user
    Register user
    load data

--- javascript/database.js
+++ javascript/database.js
@@ -15,46 +15,4 @@ function initializeDB() {
         measurementId: "G-TNEMX97BZS"
     };
     firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
 }
-//----------------------------------------------------------------------------------------
-    let db;
-    function createCollection() {
-        db = firebase.firestore();
-
-// Collection erzeugt und zu "users" gepackt
-        db.collection("users").add({
-            first: "Ada",
-            last: "Lovelace",
-            born: 1815
-        })
-            .then(function (docRef) {
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function (error) {
-                console.error("Error adding document: ", error);
-            });
-
-//weitere Collection
-        db.collection("users").add({
-            first: "Alan",
-            middle: "Mathison",
-            last: "Turing",
-            born: 1912
-        })
-            .then(function (docRef) {
-                console.log("Document written with ID: ", docRef.id);
-            })
-            .catch(function (error) {
-                console.error("Error adding document: ", error);
-            });
-    }
-
-//Daten auslesen
-    function getUsers() {
-        db.collection("users").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${doc.data()}`);
-            });
-        });
-    }
