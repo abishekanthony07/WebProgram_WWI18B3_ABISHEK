51dee1db4265718d58b1fa537f4f908ec30ed702
habjork <bjork.hannah@student.dhbw-karlsruhe.de>
2019-10-10 12:22:45 +0200

    Implementierung Funktion KJoule in Kcal umrechnen

--- javascript/kJouleUmrechner.js
+++ javascript/kJouleUmrechner.js
@@ -1,27 +1,41 @@
 window.addEventListener("load", ()=>{
    document.getElementById("button").addEventListener("click", rechne);
-});
+    document.getElementById("button1").addEventListener("click", rechne1);
 
+});
+function rechne1(){
+    let kjoulekalorien = document.getElementById('KJOULE');
+    let summekjoulekalorien = kjoulekalorien.value / 4.184;
+    console.log(summekjoulekalorien);
+    summekjoulekalorien.toFixed(2);
+    document.getElementById('output1').value = summekjoulekalorien;
+}
 
 
 function rechne() {
-
-
-
     getData("kJoule", (array) =>{
         let kilokalorien = document.getElementById('Kcal');
-        let summe  = kilokalorien.value * 4.1868;
-        summe.toFixed(2);
-        console.log(summe);
-        document.getElementById('output').value = summe;
+
+        let summekilokalorien  = kilokalorien.value * 4.1868;
+
+        summekilokalorien.toFixed(2);
+        console.log(summekilokalorien);
+        document.getElementById('output').value = summekilokalorien;
+
 
         if(array==='empty'){
             array = [{
-                kcal:kilokalorien.value
+                kcal:kilokalorien.value,
+              //  kjoule:kjoulekalorien.value,
+                summekikalorien:summekilokalorien,
+             //   summekjkalorien:summekjoulekalorien
             }]
         }else{
             array.push({
-                kcal:kilokalorien.value
+                kcal:kilokalorien.value,
+                //kjoule:kjoulekalorien.value,
+                summekikalorien:summekilokalorien,
+              //  summekjkalorien:summekjoulekalorien
             });
         }
         saveData("kJoule", array);
