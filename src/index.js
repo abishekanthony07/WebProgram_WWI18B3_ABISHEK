48b7f35e4306a1fdcdf5f315cd34bbcfed2bda51
Sascha Klevenhaus <saschaklevenhaus@gmail.com>
2019-10-22 15:28:05 +0200

    Methoden für den Login formuliert

--- src/index.js
+++ src/index.js
@@ -6,3 +6,40 @@ window.addEventListener("load", () => {
     let app = new App();
     app.start();
 });
+
+let anmeldeButton = () => {
+
+}
+
+let registerButton = () => {
+
+}
+
+let loginUser = () => {
+
+}
+
+let enterPasswordAgain = () => {
+
+}
+
+let createUser = () => {
+    firebaseAuth.createUserWithEmailAndPassword(
+        email.getText().toString();
+        password.getText().toString();
+    ).addOnSuccessListener((OnSuccessListeenr) (authResult) => {
+        user_id = Obejcts.requireNonNull(authResult.getUser()).getUid();
+        emailAdresse = authResult.getUser().getEmail();
+    }).addOnFailureListener (e) => {
+        progressBar.setVisibility(View.GONE);
+        if(e instanceof FirebaseAuthUserCollisionException) {
+            email.setError("Es existiert bereits ein Konto mit dieser Email-Adresse!");
+        } else {
+            console.log (e("Error", Objects.requireNonNull(e.getLocalizedMessage()))) ;
+        }
+    });
+}
+
+let checkEmailVerification = () => {
+
+}
